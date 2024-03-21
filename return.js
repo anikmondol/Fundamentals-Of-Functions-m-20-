1e796e9451dbce382c6f849198b4397a18c22d5f
anikmondol <anikmondol558363@gmil.com>
2024-03-21 22:52:30 +0600

    this video about how use retun in function

---
+++ return.js
@@ -0,0 +1,20 @@
+function cutHaIf(num){
+    const haIf = num / 2;
+    return haIf;
+}
+
+
+function tenTimes (number){
+    const result = number * 10;
+    return result;
+}
+
+// tenTimes(5);
+
+const result = tenTimes(5);
+console.log(result);
+
+
+const bigNumber = tenTimes(99);
+console.log(bigNumber);
+
