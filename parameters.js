9379dd82525ac1b6489fe56a20af8375f6e7eb80
anikmondol <anikmondol558363@gmil.com>
2024-03-21 21:52:07 +0600

    about this video parameters using function

---
+++ parameters.js
@@ -0,0 +1,46 @@
+function vaatkhao() {
+    console.log('hand wash kore aso');
+    console.log('boso');
+    console.log('plate e khabar dhabar nao');
+    console.log('gopat gopat kore khao');
+}
+
+
+// vaatkhao();
+
+
+// ---------> 3 * 3 = 9
+// --------> 4 * 4 = 16
+// --------> 5 * 5 = 25
+// --------> 11 * 11 = 121
+
+
+function square(numbers) {
+    console.log('value of the number parameter', numbers);
+    const borgo = numbers * numbers;
+    console.log('square of the given number is; ', borgo);
+}
+
+// square(4);
+// console.log('----------');
+// square(12);
+// console.log('-----------');
+// square(5)
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+    console.log(sum);
+}
+
+// add(10, 30);
+// add(40, 55);
+
+
+function addAll(a, b, c, d, e) {
+    const total = a + b + c + d + e;
+    console.log(a, b, c, d, e);
+    console.log(total);
+}
+
+addAll(10, 20, 30, 40)
+addAll(10, 20, 30, 40, 50)
