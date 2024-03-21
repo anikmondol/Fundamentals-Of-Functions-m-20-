17789464121c57103dd5460f73d0d6d7ea358819
anikmondol <anikmondol558363@gmil.com>
2024-03-21 21:20:06 +0600

    into to the function and function syntax

---
+++ function.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * 
+ * function name(){
+ * 
+ * 
+ * }
+ * 
+ * 
+ * **/ 
+
+// function declare
+
+function fanOffKor(){
+    console.log('bosa theke uthe dara');
+    console.log('walk toeards the switch');
+    console.log('clice the switch of the Fan');
+}
+
+
+// call the function
+fanOffKor();
+
+
+
+// function declare
+
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min and goso and fena felo');
+}
+
+// call the function
+brushTeeth();
